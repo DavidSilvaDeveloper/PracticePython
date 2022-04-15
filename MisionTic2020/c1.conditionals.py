19bfcad79767a24e13ed1a3068456d4eb0ad4c53
HernandoSilvaLeal <hernandosilvaleal@gmail.com>
2022-04-15 08:39:52 -0500

    Creacion del ambiente de trabajo profesional python + clases MT

---
+++ MisionTic2020/c1.conditionals.py
@@ -0,0 +1,45 @@
+from re import A, X
+
+
+
+x = 10
+def valor_absoluto(x):                              # Aqui estoy haciendo la buena practica snake, nombrar valores con _ entre ellos
+    if x >= 0 :
+        valor = x
+    else:
+        valor = -x
+    return valor
+print(f'El valor absoluto es: {valor_absoluto(x)}')
+
+
+
+z = 15
+a = 19
+def max_dos_numeros():
+    if a > z:
+        return a 
+    else:
+        return z
+print(f"El maximo entre los dos es: {max_dos_numeros()}")
+
+
+# Operador condicional ternario
+    
+
+
+
+
+
+
+
+
+
+
+
+# TOPIC CONDITIONALS
+# Equals: a == b
+# Not Equals: a != b
+# Less than: a < b
+# Less than or equal to: a <= b
+# Greater than: a > b
+# Greater than or equal to: a >= b
